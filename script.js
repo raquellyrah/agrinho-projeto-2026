f1b5981a04e21a244fa82b543ac22460c2c5327a
raquellyrah <raquelly.gualiume@escola.pr.gov.br>
2026-06-08 08:01:10 -0300

    projeto 1

---
+++ script.js
@@ -0,0 +1,4 @@
+// Botão de clima simples
+document.getElementById('clima-btn').addEventListener('click', () => {
+  alert('🌦 Previsão do tempo: Ensolarado com possibilidade de chuvas isoladas.');
+});
